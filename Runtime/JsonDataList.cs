5250ec0a57389f081315647b37cd10e20a5c2cf9
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-08-05 17:47:46 +0900

    refactor: stabilize json package

--- Runtime/JsonDataList.cs
+++ Runtime/JsonDataList.cs
@@ -12,6 +12,9 @@ namespace Jeomseon.Data
         [JsonProperty] internal List<T> DataList { get; set; }
 
         public IReadOnlyList<T> Data => DataList;
+        /* TODO(P2-01, api): 단순 직렬화 모델은 Unity JsonUtility로 대체 가능한지 확인하고,
+         * 다형성·사전 지원이 필요한 경우에만 Newtonsoft.Json 의존성을 유지합니다.
+         */
         internal JsonDataList() {}
     }
 }
